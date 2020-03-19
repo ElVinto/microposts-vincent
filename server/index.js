bd1a1f6788e11beb68bceff9d37adbfed72887ec
Vincent Armant <vincent.armant@insight-centre.org>
2020-03-19 10:30:54 +0100

    added static folder and SPA

--- server/index.js
+++ server/index.js
@@ -17,13 +17,22 @@ const posts = require('./routes/api/posts');
 // connect the routes of posts to server_address:port/api/posts
 app.use('/posts',posts);
 
-
 // build 
 var dataRouter = require('./routes/api/dbObservations');
 app.use('/dbObservations', dataRouter);
 
+// Handle production
+if(process.env.NODE_ENV === 'production'){
+    // Static folder 
+    app.use(express.static(__dirname+'/public'));// __dirname === .
 
-// Heroku will use pross.env.PORT otherwise locqlly we use 5000
+    // Handle Single Page application
+    // for any other routes redirect it to index.html
+    app.get(/.*/,(req,res)=> res.sendFile(__dirname+'/public/index.html'));
+}
+
+
+// Heroku will use pross.env.PORT otherwise locally we use 5000
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
